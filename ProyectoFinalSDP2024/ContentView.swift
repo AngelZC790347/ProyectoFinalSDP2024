b4b61baae1b4d36069b017b0bcb2443687530aaf
Angel <angel1200z@hotmail.com>
2024-12-07 17:51:21 -0500

    Initial Commit

---
+++ ProyectoFinalSDP2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProyectoFinalSDP2024
+//
+//  Created by Angel Zuñiga on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProyectoFinalSDP2024/ProyectoFinalSDP2024App.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoFinalSDP2024App.swift
+//  ProyectoFinalSDP2024
+//
+//  Created by Angel Zuñiga on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoFinalSDP2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
